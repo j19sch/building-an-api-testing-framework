c0eff42a02970783dacb26d5e6f9a7853a4e2e19
Joep Schuurkes <j19sch@gmail.com>
2018-06-16 19:54:03 +0200

    adds post and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# API app
+
+## Notes
+`gunicorn --reload api_app.app` (linux, mac) or `waitress-serve --port=8000 api_app:app` (win)
+`http localhost:8000`
--- api_app/fountain_pens.py
+++ api_app/fountain_pens.py
@@ -1,12 +1,10 @@
 import json
-
 import falcon
 
 
 class Resource(object):
-
-    def on_get(self, req, resp):
-        doc = {
+    def __init__(self):
+        self.doc =  {
             'images': [
                 {
                     'href': '/images/1eaf6ef1-7f2d-4ecc-a8d5-6e8adba7cc0e.png'
@@ -14,10 +12,17 @@ class Resource(object):
             ]
         }
 
+    def on_get(self, req, resp):
         # Create a JSON representation of the resource
-        resp.body = json.dumps(doc, ensure_ascii=False)
+        resp.body = json.dumps(self.doc, ensure_ascii=False)
 
         # The following line can be omitted because 200 is the default
         # status returned by the framework, but it is included here to
         # illustrate how this may be overridden as needed.
         resp.status = falcon.HTTP_200
+
+    def on_post(self, req, resp):
+        self.doc['images'].append(req.media)
+
+        resp.body = json.dumps(self.doc, ensure_ascii=False)
+        resp.status = falcon.HTTP_201
