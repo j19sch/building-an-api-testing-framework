5d6c6377c91c49d021d2e193e14f61b02ab84cd8
Joep Schuurkes <j19sch@gmail.com>
2018-11-11 09:47:55 +0100

    better printing in exercise 1's example solution

--- exercises/example_solutions/ex1_requests.py
+++ exercises/example_solutions/ex1_requests.py
@@ -1,16 +1,21 @@
 import requests
-from pprint import pformat
+from pprint import pprint
 
 
 response = requests.get('http://localhost:8000/knockknock')
-print(response.status_code, response.text, '\n')
-
+print(response.status_code)
+print(response.text)
+print("\n")
 
 response = requests.get('http://localhost:8000/books')
-print(response.status_code, response.json(), '\n')
+print(response.status_code)
+print(response.json())
+print("\n")
 
 response = requests.get('http://localhost:8000/books/8b91b84b-04e4-4496-9635-66468c2f3e41')
-print(response.status_code, pformat(response.json()), '\n')
+print(response.status_code)
+pprint((response.json()))
+print("\n")
 
 new_book = {
     "author": "Neil Gaiman",
@@ -21,7 +26,11 @@ new_book = {
     "year": 2017
 }
 response = requests.post('http://localhost:8000/books', json=new_book)
-print(response.status_code, pformat(response.json()), '\n')
+print(response.status_code)
+print(response.json())
+print("\n")
 
 response = requests.post('http://localhost:8000/token/alice')
-print(response.status_code, response.json())
+print(response.status_code)
+print(response.json())
+print("\n")
