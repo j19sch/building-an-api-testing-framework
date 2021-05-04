ded698deab6250814df4770c67f9a89642f4122d
Dmytro Titenko <dmytro.titenko@mendix.com>
2021-05-04 14:08:13 +0200

    Use test parametrization to check validation

--- exercises/test_book_api.py
+++ exercises/test_book_api.py
@@ -3,6 +3,7 @@ import jsonschema
 from . import books_client
 from . import token_client
 
+
 class TestBookApi:
     books_api = books_client.Books()
     token_api = token_client.Token()
@@ -78,8 +79,20 @@ class TestBookApi:
         response, payload = new_book
         response_body = response.json()
         assert response_body == payload
-    
+
     def test_response_has_valid_schema(self, new_book, schema_book):
         response, _ = new_book
         response_body = response.json()
         jsonschema.validate(response_body, schema_book)
+
+    @pytest.mark.parametrize("invalid_book_id, why_invalid", [
+        ("is one number string", "1"),
+        ("is a set of random characters", "aaaa"),
+        ("last character missing", "11d399cb-5a44-430c-bb9d-51fa3dab986"),
+        ("has 'h' at the beginning which makes uuid invalid", "h1d399cb-5a44-430c-bb9d-51fa3dab9864")
+    ])
+    def test_should_return_bad_request_if_id_(self, why_invalid, invalid_book_id):
+        response = self.books_api.get_book(invalid_book_id)
+        assert response.status_code == 400
+        assert response.json()['title'] == '400 Bad Request'
+        assert response.json()['description'] == 'Not a valid uuid.'
